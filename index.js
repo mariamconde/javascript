d532a19bf86e2dd6e632842ea291d39150a67ae0
mariamconde <condemriam@gmail.com>
2023-01-26 12:44:07 -0500

    learning javascript from freecodecamp

---
+++ index.js
@@ -0,0 +1,37 @@
+//comments
+var number = 5; // in-line comment
+/* this is a 
+whoop
+whoop
+whoop
+whoop
+whoop
+whoop
+whoop
+whoop
+whoop
+whoop 
+multi-line comment */
+number = 9;
+
+/* data types: 
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+//variables
+var myName = "Mariam" //used throughout whole program
+myName = 8
+let ourName = "freeCodeCamp" //only used within the scope of where you declare them
+const pi = 3.15 //variable that can never change
+
+//storing values with assignment operator
+var a; //declaring variable
+var b = 2; //declaring and assigning variables
+console.log(a);
+
+a = 7;
+b = a;
+console.log(a); 
+
+//initialing variables with assignment operators
+var a = 9;
